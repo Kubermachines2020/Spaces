a3e1ebec714e41e15854f7c164f7c87053ec81c1
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-02-22 16:33:59 +0100

    Update space.sh

--- space.sh
+++ space.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source
 alias kubermachins_configure='curl -fsSL   https://raw.githubusercontent.com/Kubermachines2020/Spaces/main/install.sh  | bash'
 alias kubermachins_up='curl -fsSL  https://raw.githubusercontent.com/Kubermachines2020/Spaces/main/up.sh  | bash'
