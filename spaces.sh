e8a813ae34f44f26aabb92bcf64224ca302893ff
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-02-22 16:39:10 +0100

    Create spaces.sh

---
+++ spaces.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+alias kubermachins_configure='curl -fsSL   https://raw.githubusercontent.com/Kubermachines2020/Spaces/main/install.sh  | bash'
+alias kubermachines_up='curl -fsSL  https://raw.githubusercontent.com/Kubermachines2020/Spaces/main/up.sh  | bash'
