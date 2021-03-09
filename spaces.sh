34975f136efe2347618949d9fe4e4e925478b007
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-03-09 14:57:19 +0100

    Update and rename spaces.sh to init.sh

--- spaces.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-alias kubermachins_configure='curl -fsSL   https://raw.githubusercontent.com/Kubermachines2020/Spaces/main/install.sh  | bash'
-alias kubermachines_up='curl -fsSL  https://raw.githubusercontent.com/Kubermachines2020/Spaces/main/up.sh  | bash'
