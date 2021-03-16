dd901d40f1443420755b3e6e574e67321672a45a
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-03-16 02:42:54 -1100

    Create kubermachines.ps1

---
+++ kubermachines.ps1
@@ -0,0 +1,33 @@
+param(
+  [parameter(Mandatory=$true)]
+  [String] 
+  $flag
+)
+
+Process {
+
+
+  if($flag -ceq "install") {
+
+           iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/Kubermachines2020/Spaces/windows/install.ps1'))
+
+  } elseif($flag -ceq "configure"){
+
+           iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/Kubermachines2020/Spaces/windows/configure.ps1'))
+
+  } elseif($flag -ceq "up"){
+
+          tilt up
+
+  } elseif($flag -ceq "down"){
+  
+          tilt down
+          
+  } elseif($flag -ceq "help"){
+
+          "help"
+
+  } else {"kubermachines: '$flag' is not a kubermachines command. See 'kubermachines help' for usage."}
+
+
+ }
