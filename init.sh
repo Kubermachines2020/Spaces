bd3616994f12513d00b8922a9551635c0e2ab803
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-03-16 02:43:45 -1100

    Delete init.sh

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# [STEPS] Kubermachines command 
-cp kubermachines /usr/local/bin/
-chmod +x /usr/local/bin/kubermachines
-printf "\nif [[ -f /usr/local/bin/kubermachines ]]; then\n    source /usr/local/bin/kubermachines\nfi" >> $HOME/.bashrc
-source $HOME/.bashrc
