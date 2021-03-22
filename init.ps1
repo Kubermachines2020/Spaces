d081f650842da0d8f88dba674480c2ba76adbf41
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-03-22 14:46:35 +0100

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,2 +1,6 @@
 Copy-Item $home\kubermachines.ps1 -Destination $home\bin\kubermachines.ps1
 Set-Alias kubermachines "$HOME\bin\kubermachines.ps1" -Scope Global
+.\setx /M path "%path%;$HOME\bin"
+
+Write-Output "Kubermachines installed!"
+Write-Output "Run 'kubermachines up' to start."
