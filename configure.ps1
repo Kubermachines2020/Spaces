545e6527d09ce59a11e4dc1681c0e563e35aba01
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-03-22 21:18:01 -1100

    Update configure.ps1

--- configure.ps1
+++ configure.ps1
@@ -3,9 +3,9 @@
 # Usage:
 #   iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/Kubermachines2020/Spaces/windows/configure.ps1')
 
-Copy-Item $contextfile -Destination $home\.kube\config
+Copy-Item $env:contextfile -Destination $home\.kube\config
 
-$context = Get-Content $contextfile | Select-String -Pattern '^current-context' | ForEach-Object {
+$context = Get-Content $env:contextfile | Select-String -Pattern '^current-context' | ForEach-Object {
     -split $_.Line |Select-Object -Last 1
 }
 
