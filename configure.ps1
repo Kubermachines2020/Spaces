1ece7bc73ada6237b2dbd1bb413781f451c95b73
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-03-22 22:07:26 -1100

    Update configure.ps1

--- configure.ps1
+++ configure.ps1
@@ -4,6 +4,7 @@
 #   iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/Kubermachines2020/Spaces/windows/configure.ps1')
 Write-Host " You are using $env:contextfile file .."
 $contextfilepath = Get-Content $env:contextfile
+Write-Host $contextfilepath
 Copy-Item $contextfilepath -Destination $home\.kube\config
 
 $context = Get-Content $contextfilepath | Select-String -Pattern '^current-context' | ForEach-Object {
