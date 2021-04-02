5d64de1a9cd3f8670a90f367a3eb8ffbb9a5d939
Kubermachines2020 <67432031+Kubermachines2020@users.noreply.github.com>
2021-04-02 08:58:13 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 PWD=$(grep PWD .cred | xargs)
 PWD=${PWD#*=}
+
 USERNAME=$(grep USERNAME .cred | xargs)
 USERNAME=${USERNAME#*=}
+
 REGISTRY=$(grep REGISTRY .cred | xargs)
 REGISTRY=${REGISTRY#*=}
 
+DEPLOYMENT=$(grep DEPLOYMENT .cred | xargs)
+DEPLOYMENT=${DEPLOYMENT#*=}
+
+NAMESPACE=$(grep NAMESPACE .cred | xargs)
+NAMESPACE=${NAMESPACE#*=}
+
 function kubermachines_configure (){
   echo $PWD | docker login --username $USERNAME --password-stdin $REGISTRY
-  mkdir -p /root/.kube
   cp $CONTEXT_FILE ~/.kube/config
+  kubectl create secret docker-registry $DEPLOYMENT --docker-server=https://index.docker.io/v2/ --docker-username=$USERNAME --docker-password=$PWD -n $NAMESPACE
 }
 
 kubermachines_configure
